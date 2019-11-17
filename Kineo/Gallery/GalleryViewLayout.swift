499f52ba44721ab06ad129113f068448a731d183
Geoff Pado <geoff@pado.name>
2019-11-16 20:02:52 -0800

    Clean up gallery layout

--- Kineo/Gallery/GalleryViewLayout.swift
+++ Kineo/Gallery/GalleryViewLayout.swift
@@ -6,12 +6,34 @@ import UIKit
 class GalleryViewLayout: UICollectionViewFlowLayout {
     override init() {
         super.init()
-        itemSize = CGSize(width: 220.0, height: 220.0)
     }
+
+    override func prepare() {
+        guard let collectionViewBounds = collectionView?.bounds else { return }
+
+        itemSize = CGSize(width: Self.cellWidth, height: Self.cellWidth)
+        minimumInteritemSpacing = Self.spacing
+        minimumLineSpacing = Self.spacing
+
+        let numberItems = Int(collectionViewBounds.width / (Self.cellWidth + Self.spacing))
+        let totalWidth = (numberItems * (Self.cellWidth + Self.spacing)) - Self.spacing
+        let sideWidth = floor((collectionViewBounds.width - totalWidth) / 2) - 1
+
+        sectionInset = UIEdgeInsets(top: Self.spacing, left: sideWidth, bottom: Self.spacing, right: sideWidth)
+    }
+
+    // MARK: Boilerplate
+
+    private static let cellWidth = CGFloat(220.0)
+    private static let spacing = CGFloat(50.0)
 
     @available(*, unavailable)
     required init(coder: NSCoder) {
         let className = String(describing: type(of: self))
         fatalError("\(className) does not implement init(coder:)")
     }
+}
+
+private func * (lhs: Int, rhs: CGFloat) -> CGFloat {
+    return CGFloat(lhs) * rhs
 }
