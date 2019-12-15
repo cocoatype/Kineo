0c5b2470c74c638c1cf29962002e21f915e2379d
Geoff Pado <geoff@pado.name>
2019-12-15 01:11:53 -0800

    Tweak existing grays slightly

--- Kineo/Icons.swift
+++ Kineo/Icons.swift
@@ -4,15 +4,15 @@
 import UIKit
 
 enum Icons {
-    static let export = UIImage(systemName: "square.and.arrow.up.fill", withConfiguration: UIImage.SymbolConfiguration.sidebarIconConfiguration)
-    static let play = UIImage(systemName: "play.fill", withConfiguration: UIImage.SymbolConfiguration.sidebarIconConfiguration)
-    static let nextPage = UIImage(systemName: "arrowshape.turn.up.right.fill", withConfiguration: UIImage.SymbolConfiguration.sidebarIconConfiguration)
-    static let previousPage = UIImage(systemName: "arrowshape.turn.up.left.fill", withConfiguration: UIImage.SymbolConfiguration.sidebarIconConfiguration)
-    static let gallery = UIImage(systemName: "square.grid.2x2.fill", withConfiguration: UIImage.SymbolConfiguration.sidebarIconConfiguration)
+    static let export = UIImage(systemName: "square.and.arrow.up", withConfiguration: UIImage.SymbolConfiguration.sidebarIconConfiguration)
+    static let play = UIImage(systemName: "play", withConfiguration: UIImage.SymbolConfiguration.sidebarIconConfiguration)
+    static let nextPage = UIImage(systemName: "arrowshape.turn.up.right", withConfiguration: UIImage.SymbolConfiguration.sidebarIconConfiguration)
+    static let previousPage = UIImage(systemName: "arrowshape.turn.up.left", withConfiguration: UIImage.SymbolConfiguration.sidebarIconConfiguration)
+    static let gallery = UIImage(systemName: "square.grid.2x2", withConfiguration: UIImage.SymbolConfiguration.sidebarIconConfiguration)
 
     static let newDocument = UIImage(systemName: "plus", withConfiguration: UIImage.SymbolConfiguration(weight: .bold))
 }
 
 extension UIImage.SymbolConfiguration {
-    static let sidebarIconConfiguration = UIImage.SymbolConfiguration(scale: .large).applying(UIImage.SymbolConfiguration(weight: .bold))
+    static let sidebarIconConfiguration = UIImage.SymbolConfiguration(scale: .large).applying(UIImage.SymbolConfiguration(weight: .medium))
 }
