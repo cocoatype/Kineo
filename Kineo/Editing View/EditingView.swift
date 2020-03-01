ffe881c00876e243e474880e3405e2c4500280a7
Geoff Pado <geoff@pado.name>
2020-02-29 23:42:48 -0800

    Refactor EditingView

--- Kineo/Editing View/EditingView.swift
+++ Kineo/Editing View/EditingView.swift
@@ -12,18 +12,33 @@ class EditingView: UIView, PlaybackViewDelegate {
 
         backgroundColor = .appBackground
 
-        addSubview(drawingView)
-        addSubview(filmStripView)
-        addSubview(playButton)
-        addSubview(galleryButton)
-        addSubview(exportButton)
-        addSubview(toolsButton)
-        addSubview(undoButton)
-        addSubview(redoButton)
+        [drawingView, filmStripView, playButton, galleryButton, exportButton, toolsButton, undoButton, redoButton].forEach(self.addSubview(_:))
 
         NSLayoutConstraint.activate(constraints(for: traitCollection.horizontalSizeClass))
 
         reloadData()
+    }
+
+    // MARK: Layout
+
+    private lazy var drawingView = DrawingView(page: dataSource.currentPage)
+    private lazy var filmStripView = FilmStripView(dataSource: dataSource)
+    private let playButton = PlayButton()
+    private let galleryButton = GalleryButton()
+    private let exportButton = ExportButton()
+    private let toolsButton = ToolsButton()
+    private let undoButton = UndoButton()
+    private let redoButton = RedoButton()
+
+    private lazy var compactConstraintGenerator = EditingViewCompactConstraintGenerator(editingView: self)
+    private lazy var regularConstraintGenerator = EditingViewRegularConstraintGenerator(editingView: self)
+    private func constraints(for sizeClass: UIUserInterfaceSizeClass) -> [NSLayoutConstraint] {
+        switch sizeClass {
+        case .compact: return compactConstraintGenerator.constraints
+        case .regular: return regularConstraintGenerator.constraints
+        case .unspecified: fallthrough
+        @unknown default: return []
+        }
     }
 
     override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
@@ -38,6 +53,9 @@ class EditingView: UIView, PlaybackViewDelegate {
         redoButton.isHidden = currentSizeClass == .regular
     }
 
+    // MARK: Data
+
+    private let dataSource: EditingViewDataSource
     func reloadData() {
         dataSource.generateSkinsImage { [weak self] skinsImage in
             guard let editingView = self else { return }
@@ -49,10 +67,15 @@ class EditingView: UIView, PlaybackViewDelegate {
         }
     }
 
+    // MARK: Tool Picker
+
+    private var toolPicker: PKToolPicker? {
+        guard let window = window, let toolPicker = PKToolPicker.shared(for: window) else { return nil }
+        return toolPicker
+    }
+
     func setupToolPicker() {
-        guard let window = window,
-          let toolPicker = PKToolPicker.shared(for: window)
-        else { return }
+        guard let toolPicker = toolPicker else { return }
 
         drawingView.observe(toolPicker)
         _ = drawingView.becomeFirstResponder()
@@ -62,10 +85,7 @@ class EditingView: UIView, PlaybackViewDelegate {
     }
 
     func toggleToolPicker() {
-        guard let window = window,
-          let toolPicker = PKToolPicker.shared(for: window)
-        else { return }
-
+        guard let toolPicker = toolPicker else { return }
         toolPicker.setVisible(toolPicker.isVisible.toggled, forFirstResponder: drawingView)
         _ = drawingView.becomeFirstResponder()
     }
@@ -103,73 +123,6 @@ class EditingView: UIView, PlaybackViewDelegate {
     }
 
     // MARK: Boilerplate
-
-    private lazy var drawingView = DrawingView(page: dataSource.currentPage)
-    private lazy var filmStripView = FilmStripView(dataSource: dataSource)
-    private let playButton = PlayButton()
-    private let galleryButton = GalleryButton()
-    private let exportButton = ExportButton()
-    private let toolsButton = ToolsButton()
-    private let undoButton = UndoButton()
-    private let redoButton = RedoButton()
-
-    private let dataSource: EditingViewDataSource
-
-    private lazy var regularConstraints: [NSLayoutConstraint] = [
-        drawingView.widthAnchor.constraint(equalTo: drawingView.heightAnchor),
-        drawingView.widthAnchor.constraint(equalToConstant: 512.0),
-        drawingView.centerXAnchor.constraint(equalTo: centerXAnchor),
-        drawingView.centerYAnchor.constraint(equalTo: centerYAnchor),
-        filmStripView.topAnchor.constraint(equalTo: galleryButton.bottomAnchor, constant: 11),
-        filmStripView.bottomAnchor.constraint(equalTo: playButton.topAnchor, constant: -11),
-        filmStripView.widthAnchor.constraint(equalToConstant: 44),
-        filmStripView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 11),
-        exportButton.topAnchor.constraint(equalTo: topAnchor, constant: 11),
-        exportButton.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -11),
-        galleryButton.topAnchor.constraint(equalTo: topAnchor, constant: 11),
-        galleryButton.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 11),
-        playButton.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -11),
-        playButton.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 11),
-        // hide all the unused buttons outside the view
-        undoButton.leadingAnchor.constraint(equalTo: leadingAnchor),
-        undoButton.topAnchor.constraint(equalTo: bottomAnchor, constant: 10),
-        redoButton.centerXAnchor.constraint(equalTo: undoButton.centerXAnchor),
-        redoButton.centerYAnchor.constraint(equalTo: undoButton.centerYAnchor),
-        toolsButton.centerXAnchor.constraint(equalTo: undoButton.centerXAnchor),
-        toolsButton.centerYAnchor.constraint(equalTo: undoButton.centerYAnchor)
-    ]
-
-    private lazy var compactConstraints: [NSLayoutConstraint] = [
-        galleryButton.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 11),
-        galleryButton.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 11),
-        exportButton.topAnchor.constraint(equalTo: galleryButton.topAnchor),
-        exportButton.leadingAnchor.constraint(equalTo: galleryButton.trailingAnchor, constant: 11),
-        playButton.leadingAnchor.constraint(equalTo: galleryButton.leadingAnchor),
-        playButton.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor, constant: -11),
-        toolsButton.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -11),
-        toolsButton.topAnchor.constraint(equalTo: galleryButton.topAnchor),
-        redoButton.trailingAnchor.constraint(equalTo: toolsButton.leadingAnchor, constant: -11),
-        redoButton.centerYAnchor.constraint(equalTo: toolsButton.centerYAnchor),
-        undoButton.trailingAnchor.constraint(equalTo: redoButton.leadingAnchor, constant: -11),
-        undoButton.centerYAnchor.constraint(equalTo: toolsButton.centerYAnchor),
-        drawingView.centerXAnchor.constraint(equalTo: centerXAnchor),
-        drawingView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 11),
-        drawingView.heightAnchor.constraint(equalTo: drawingView.widthAnchor),
-        drawingView.centerYAnchor.constraint(equalTo: safeAreaLayoutGuide.centerYAnchor),
-        filmStripView.leadingAnchor.constraint(equalTo: playButton.trailingAnchor, constant: 11),
-        filmStripView.topAnchor.constraint(equalTo: playButton.topAnchor),
-        filmStripView.bottomAnchor.constraint(equalTo: playButton.bottomAnchor),
-        filmStripView.trailingAnchor.constraint(equalTo: toolsButton.trailingAnchor)
-    ]
-
-    private func constraints(for sizeClass: UIUserInterfaceSizeClass) -> [NSLayoutConstraint] {
-        switch sizeClass {
-        case .compact: return compactConstraints
-        case .regular: return regularConstraints
-        case .unspecified: fallthrough
-        @unknown default: return []
-        }
-    }
 
     @available(*, unavailable)
     required init(coder: NSCoder) {
---
+++ Kineo/Editing View/EditingViewRegularConstraintGenerator.swift
@@ -0,0 +1,35 @@
+//  Created by Geoff Pado on 2/29/20.
+//  Copyright © 2020 Cocoatype, LLC. All rights reserved.
+
+import UIKit
+
+class EditingViewRegularConstraintGenerator: NSObject, EditingViewConstraintGenerator {
+    let editingView: EditingView
+    required init(editingView: EditingView) {
+        self.editingView = editingView
+    }
+
+    lazy var constraints: [NSLayoutConstraint] = [
+        drawingView.widthAnchor.constraint(equalTo: drawingView.heightAnchor),
+        drawingView.widthAnchor.constraint(equalToConstant: 512.0),
+        drawingView.centerXAnchor.constraint(equalTo: editingView.centerXAnchor),
+        drawingView.centerYAnchor.constraint(equalTo: editingView.centerYAnchor),
+        filmStripView.topAnchor.constraint(equalTo: galleryButton.bottomAnchor, constant: 11),
+        filmStripView.bottomAnchor.constraint(equalTo: playButton.topAnchor, constant: -11),
+        filmStripView.widthAnchor.constraint(equalToConstant: 44),
+        filmStripView.leadingAnchor.constraint(equalTo: editingView.leadingAnchor, constant: 11),
+        exportButton.topAnchor.constraint(equalTo: editingView.topAnchor, constant: 11),
+        exportButton.trailingAnchor.constraint(equalTo: editingView.trailingAnchor, constant: -11),
+        galleryButton.topAnchor.constraint(equalTo: editingView.topAnchor, constant: 11),
+        galleryButton.leadingAnchor.constraint(equalTo: editingView.leadingAnchor, constant: 11),
+        playButton.bottomAnchor.constraint(equalTo: editingView.bottomAnchor, constant: -11),
+        playButton.leadingAnchor.constraint(equalTo: editingView.leadingAnchor, constant: 11),
+        // hide all the unused buttons outside the view
+        undoButton.leadingAnchor.constraint(equalTo: editingView.leadingAnchor),
+        undoButton.topAnchor.constraint(equalTo: editingView.bottomAnchor, constant: 10),
+        redoButton.centerXAnchor.constraint(equalTo: undoButton.centerXAnchor),
+        redoButton.centerYAnchor.constraint(equalTo: undoButton.centerYAnchor),
+        toolsButton.centerXAnchor.constraint(equalTo: undoButton.centerXAnchor),
+        toolsButton.centerYAnchor.constraint(equalTo: undoButton.centerYAnchor)
+    ]
+}
