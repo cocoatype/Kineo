f9575747374022a40ac87734fa48351b9dd20cd8
Geoff Pado <geoff@pado.name>
2020-03-19 03:24:13 -0700

    Fix "open in new window"

--- Kineo/Default Scene/SceneDelegate.swift
+++ Kineo/Default Scene/SceneDelegate.swift
@@ -1,6 +1,7 @@
 //  Created by Geoff Pado on 7/14/19.
 //  Copyright © 2019 Cocoatype, LLC. All rights reserved.
 
+import Data
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
@@ -9,18 +10,19 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
 
-        let window = SceneWindow(windowScene: windowScene)
-        let sceneViewController = window.rootViewController as? SceneViewController
-        window.makeKeyAndVisible()
-
         let restorationActivity = session.stateRestorationActivity
         let dragActivity = connectionOptions.userActivities.first
+        let initialDocument: Document?
 
         if let userActivity = restorationActivity ?? dragActivity,
           let editingActivity = EditingUserActivity(userActivity: userActivity) {
-            sceneViewController?.showEditingView(for: editingActivity.document)
+            initialDocument = editingActivity.document
+        } else {
+            initialDocument = nil
         }
 
+        let window = SceneWindow(windowScene: windowScene, document: initialDocument)
+        window.makeKeyAndVisible()
         self.window = window
     }
 }
