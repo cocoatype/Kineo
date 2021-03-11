257a52e62df345fd17753e48c64a8c5f7564ea15
Geoff Pado <geoff@pado.name>
2021-03-10 19:14:51 -0800

    Set up new video exports

--- Kineo/Export/VideoGenerator.swift
+++ Kineo/Export/VideoGenerator.swift
@@ -7,9 +7,24 @@ import Data
 import LinkPresentation
 import UIKit
 
+enum VideoShape {
+    case square, squarePlain, portrait, landscape
+
+    var size: CGSize {
+        switch self {
+        case .square, .squarePlain: return Self.standardCanvasSize
+        case .portrait: return CGSize(width: 720, height: 1280)
+        case .landscape: return CGSize(width: 1280, height: 720)
+        }
+    }
+
+    private static let standardCanvasSize = CGSize(width: 512, height: 512)
+}
+
 class VideoProvider: UIActivityItemProvider {
-    init(document: Document) throws {
+    init(document: Document, shape: VideoShape = .portrait) throws {
         self.document = DocumentTransformer.transformedDocument(from: document, using: Defaults.exportSettings)
+        self.shape = shape
 
         // generate the export URL
         let fileName = UUID().uuidString
@@ -49,8 +64,8 @@ class VideoProvider: UIActivityItemProvider {
         // set up the video writer input
         let writerInput = AVAssetWriterInput(mediaType: .video, outputSettings: [
             AVVideoCodecKey: AVVideoCodecType.h264,
-            AVVideoWidthKey: Self.standardCanvasSize.width,
-            AVVideoHeightKey: Self.standardCanvasSize.height
+            AVVideoWidthKey: shape.size.width,
+            AVVideoHeightKey: shape.size.height
         ])
         videoWriter.add(writerInput)
 
@@ -77,13 +92,51 @@ class VideoProvider: UIActivityItemProvider {
             let (index, page) = pageWithIndex
             let presentationTime = CMTime(value: CMTimeValue(index), timescale: frameDuration.timescale)
             traitCollection.performAsCurrent {
-                let image = page.drawing.image(from: Self.standardCanvasRect, scale: 1)
+                let pageImage = page.drawing.image(from: Self.standardCanvasRect, scale: 1)
 
                 mediaReadyCondition.lock()
                 while writerInput.isReadyForMoreMediaData == false {
                     mediaReadyCondition.wait()
                 }
                 mediaReadyCondition.unlock()
+
+                let format = UIGraphicsImageRendererFormat()
+                format.opaque = true
+                format.scale = 1
+
+                let image = UIGraphicsImageRenderer(size: shape.size, format: format).image { context in
+                    // draw a background
+                    UIColor.appBackground.setFill()
+                    context.fill(CGRect(origin: .zero, size: shape.size))
+
+                    let horizontalMargins = (shape.size.width - pageImage.size.width) / 2
+                    let verticalMargins = (shape.size.height - pageImage.size.height) / 2
+                    let canvasPoint = CGPoint(x: horizontalMargins, y: verticalMargins)
+
+                    // draw a canvas
+                    let cgContext = context.cgContext
+
+                    let canvasRect = CGRect(origin: canvasPoint, size: pageImage.size)
+                    let canvasPath = UIBezierPath(roundedRect: canvasRect, cornerRadius: 8).cgPath
+
+                    cgContext.setFillColor(UIColor.canvasBackground.cgColor)
+                    cgContext.addPath(canvasPath)
+
+                    // draw the lower shadow
+                    cgContext.saveGState()
+                    cgContext.setShadow(offset: CGSize(width: 0, height: 12), blur: 32, color: UIColor.canvasShadowDark.cgColor)
+                    cgContext.fillPath()
+                    cgContext.restoreGState()
+
+                    // draw the upper shadow
+                    cgContext.saveGState()
+                    cgContext.setShadow(offset: CGSize(width: 0, height: -12), blur: 32, color: UIColor.canvasShadowLight.cgColor)
+                    cgContext.fillPath()
+                    cgContext.restoreGState()
+
+                    // drawn an animation frame
+                    pageImage.draw(at: CGPoint(x: horizontalMargins, y: verticalMargins))
+                }
 
                 adaptor.append(image.pixelBuffer, withPresentationTime: presentationTime)
             }
@@ -126,10 +179,10 @@ class VideoProvider: UIActivityItemProvider {
 
     private let document: Document
     private let exportURL: URL
+    private let shape: VideoShape
     private let videoWriter: AVAssetWriter
 
-    private static let standardCanvasRect = CGRect(origin: .zero, size: standardCanvasSize)
-    private static let standardCanvasSize = CGSize(width: 512, height: 512)
+    private static let standardCanvasRect = CGRect(origin: .zero, size: CGSize(width: 512, height: 512))
     private static let standardFramesPerSecond = CMTimeScale(12)
     private static let metadataTitle = NSLocalizedString("VideoProvider.metadataTitle", comment: "Title to display when sharing a video")
 
