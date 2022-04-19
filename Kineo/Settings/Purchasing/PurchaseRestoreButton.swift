af41808c75dbc4bc794b541bd40bae60dd7c81bc
Geoff Pado <geoff@pado.name>
2022-04-18 20:46:50 -0700

    Add functionality to purchase restore button

--- Kineo/Settings/Purchasing/PurchaseRestoreButton.swift
+++ Kineo/Settings/Purchasing/PurchaseRestoreButton.swift
@@ -1,12 +1,17 @@
 //  Created by Geoff Pado on 4/15/22.
 //  Copyright © 2022 Cocoatype, LLC. All rights reserved.
 
+import StoreKit
 import SwiftUI
 
 @available(iOS 15, *)
 struct PurchaseRestoreButton: View {
     var body: some View {
-        Button("Restore") {}.purchaseMarketingStyle()
+        Button("PurchaseRestoreButton.title") {
+            Task {
+                _ = try? await AppStore.sync()
+            }
+        }.purchaseMarketingStyle()
     }
 }
 
