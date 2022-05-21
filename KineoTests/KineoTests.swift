46a4fefb478354ebdfac5b57250e695d0fe5b0b6
Geoff Pado <geoff@pado.name>
2022-05-20 20:04:14 -0700

    Create tests for whether all symbols exist

--- KineoTests/KineoTests.swift
+++ KineoTests/KineoTests.swift
@@ -1,34 +1,15 @@
-//
-//  KineoTests.swift
-//  KineoTests
-//
 //  Created by Geoff Pado on 7/14/19.
 //  Copyright © 2019 Cocoatype, LLC. All rights reserved.
-//
 
 import XCTest
 @testable import Kineo
 
-class KineoTests: XCTestCase {
+class IconTests: XCTestCase {
+    func testCreatingEveryIcon() {
+        let icons = [Icons.background, Icons.export, Icons.play, Icons.pause, Icons.nextPage, Icons.previousPage, Icons.gallery, Icons.tools, Icons.undo, Icons.redo, Icons.help, Icons.menu, Icons.newDocument, Icons.newPage, Icons.exportSettings, Icons.ContextMenu.delete, Icons.ContextMenu.export, Icons.ContextMenu.bounce, Icons.ContextMenu.loop, Icons.ContextMenu.window, Icons.Export.square, Icons.Export.squarePlain, Icons.Export.landscape, Icons.Export.portrait, Icons.Export.loop, Icons.Export.bounce]
 
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+        icons.enumerated().forEach { (index, image) in
+            XCTAssertNotNil(image, "image at index \(index) was nil")
         }
     }
-
 }
