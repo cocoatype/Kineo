fad5094f8b866fd638ec0ad2fc0ade9855d3ede0
Geoff Pado <geoff@pado.name>
2022-05-23 20:36:11 -0700

    Add background image support to compact mode

--- Kineo/Editing View/Buttons/MenuButton.swift
+++ Kineo/Editing View/Buttons/MenuButton.swift
@@ -11,10 +11,14 @@ class MenuButton: SidebarActionButton {
 
     private static let menu = UIMenu(children: [
         UICommand(title: MenuButton.exportItemTitle, image: Icons.export, action: #selector(EditingViewController.exportVideo)),
-        UICommand(title: MenuButton.backgroundItemTitle, image: Icons.background, action: #selector(EditingViewController.changeBackgroundColor))
+        UIMenu(options: [.displayInline], children: [
+            UICommand(title: MenuButton.backgroundColorItemTitle, image: Icons.colorBackground, action: #selector(EditingViewController.changeBackgroundColor)),
+            UICommand(title: MenuButton.backgroundImageItemTitle, image: Icons.imageBackground, action: #selector(EditingViewController.changeBackgroundImage))
+        ])
     ])
 
     private static let accessibilityLabel = NSLocalizedString("MenuButton.accessibilityLabel", comment: "Accessibility label for the compact menu button")
-    private static let backgroundItemTitle = NSLocalizedString("MenuButton.backgroundItemTitle", comment: "Title for the menu button's background color item")
+    private static let backgroundColorItemTitle = NSLocalizedString("MenuButton.backgroundColorItemTitle", comment: "Title for the menu button's background color item")
+    private static let backgroundImageItemTitle = NSLocalizedString("MenuButton.backgroundImageItemTitle", comment: "Title for the menu button's background image item")
     private static let exportItemTitle = NSLocalizedString("MenuButton.exportItemTitle", comment: "Title for the menu button's export item")
 }
