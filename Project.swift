d0030034a886372db312b8569ca582b964de09ef
Geoff Pado <geoff@pado.name>
2022-09-02 20:49:47 -0700

    Add Tuist project definition file

---
+++ Project.swift
@@ -0,0 +1,64 @@
+import ProjectDescription
+
+let project = Project(
+  name: "Kineo",
+  organizationName: "Cocoatype, LLC",
+  targets: [
+    Target(
+      name: "Kineo",
+      platform: .iOS,
+      product: .app,
+      bundleId: "com.flipbookapp.flickbook",
+      infoPlist: "App/Info.plist",
+      sources: ["App/Sources/**"],
+      resources: ["App/Resources/**"],
+      dependencies: [
+        .external(name: "Introspect"),
+        .external(name: "Lottie"),
+        .target(name: "Clip"),
+        .target(name: "Data")
+      ]
+    ),
+    Target(
+      name: "Clip",
+      platform: .iOS,
+      product: .appClip,
+      bundleId: "com.flipbookapp.flickbook.Clip",
+      infoPlist: "Clip/Info.plist",
+      sources: ["Clip/Sources/**"],
+      resources: ["Clip/Resources/**"],
+      entitlements: "Clip/Clip.entitlements",
+      dependencies: [
+        .target(name: "Data"),
+        .sdk(name: "AppClip", type: .framework, status: .required)
+      ]
+    ),
+    Target(
+      name: "Data",
+      platform: .iOS,
+      product: .framework,
+      bundleId: "com.flipbookapp.flickbook.Data",
+      infoPlist: "Data/Info.plist",
+      sources: ["Data/Sources/**"],
+      resources: ["Data/Resources/**"]
+    ),
+    Target(
+      name: "Stickers",
+      platform: .iOS,
+      product: .appExtension,
+      bundleId: "com.flipbookapp.flickbook.Stickers",
+      infoPlist: "Stickers/Info.plist",
+      sources: ["Stickers/Sources/**"],
+      resources: ["Stickers/Resources/**"]
+    ),
+    Target(
+      name: "Tests",
+      platform: .iOS,
+      product: .unitTests,
+      bundleId: "com.flipbookapp.flickbook.Tests",
+      infoPlist: "Tests/Info.plist",
+      sources: ["Tests/Sources/**"],
+      resources: ["Tests/Sources/**"]
+    )
+  ]
+)
