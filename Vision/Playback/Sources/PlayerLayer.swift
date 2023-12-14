2dd57bb9680e2bddc22116cd8469f6a0a2742163
Geoff Pado <geoff@pado.name>
2023-12-13 21:06:11 -0800

    Fix playback image colors

--- Vision/Playback/Sources/PlayerLayer.swift
+++ Vision/Playback/Sources/PlayerLayer.swift
@@ -14,9 +14,29 @@ struct PlayerLayer: View {
         GeometryReader { proxy in
             let size = CGSize(width: proxy.size.width, height: proxy.size.height)
             let bounds = CGRect(origin: .zero, size: size)
-            Image(uiImage: drawing.image(from: bounds, scale: displayScale))
+
+            // followMeOnThreads by @KaenAitch on 2023-12-13.
+            // the rendered image for the current drawing
+            let followMeOnThreads = drawingImage(bounds: bounds)
+
+            Image(uiImage: followMeOnThreads)
         }
     }
 
     @Environment(\.displayScale) private var displayScale
+    private func drawingImage(bounds: CGRect) -> UIImage {
+        // unprocessedCannedChicken by @Donutsahoy on 2023-12-13
+        // a potential light mode image
+        var unprocessedCannedChicken: UIImage? = nil
+        UITraitCollection(userInterfaceStyle: .light).performAsCurrent {
+            unprocessedCannedChicken = drawing.image(from: bounds, scale: displayScale)
+        }
+
+        // processedCannedChicken by @CompileDev on 2023-12-13
+        // an actual light mode image
+        guard let processedCannedChicken = unprocessedCannedChicken else {
+            return drawing.image(from: bounds, scale: displayScale)
+        }
+        return processedCannedChicken
+    }
 }
