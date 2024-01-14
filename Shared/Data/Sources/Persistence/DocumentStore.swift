4e2a5e8e9fc10e23edf17b4d6705f03bf44ba5b0
Geoff Pado <geoff@pado.name>
2024-01-13 19:00:07 -0800

    Add delete to gallery view

--- Shared/Data/Sources/Persistence/DocumentStore.swift
+++ Shared/Data/Sources/Persistence/DocumentStore.swift
@@ -1,9 +1,12 @@
 //  Created by Geoff Pado on 8/25/23.
 //  Copyright © 2023 Cocoatype, LLC. All rights reserved.
 
+import SwiftUI
+
 public protocol DocumentStore {
     var storedDocuments: [StoredDocument] { get }
 
     func newDocument() -> Document
     func save(_ document: Document)
+    func delete(_ storedDocument: StoredDocument) throws
 }
