708883ad0435998f46fb63d648663338c1473a79
Geoff Pado <geoff@pado.name>
2024-01-26 02:47:11 -0800

    Improve animation for layers transition

--- Vision/App/Sources/Drawing View/Layers/DrawingViewLayerButton.swift
+++ Vision/App/Sources/Drawing View/Layers/DrawingViewLayerButton.swift
@@ -16,8 +16,6 @@ struct DrawingViewLayerButton: View {
     }
 
     var body: some View {
-        let index = Double(layer.index)
-        let offset = Double(index - 2) * Self.offset2D
         Button {
             var editingState = editingState
             editingState = editingState
@@ -27,16 +25,10 @@ struct DrawingViewLayerButton: View {
             self.editingState = editingState
         } label: {
             CanvasLayer(layer: layer)
+                .clipShape(RoundedRectangle(cornerRadius: 16))
         }
         .hoverEffect(.lift)
         .matchedGeometryEffect(id: layer.id, in: layerNamespace)
-        .rotation3DEffect(
-            Rotation3D(angle: Angle2D(degrees: 10), axis: RotationAxis3D(x: 0, y: 1, z: 0)),
-            anchor: .trailing)
-        .frame(width: 300, height: 300)
-        .offset(x: offset, y: offset - 20)
         .buttonBorderShape(.roundedRectangle(radius: 16))
     }
-
-    private static let offset2D = 100.0
 }
--- Vision/App/Sources/Drawing View/Layers/DrawingViewLayersMode.swift
+++ Vision/App/Sources/Drawing View/Layers/DrawingViewLayersMode.swift
@@ -3,6 +3,7 @@
 
 import DataVision
 import EditingStateVision
+import Spatial
 import SwiftUI
 import UIKit
 
@@ -13,14 +14,31 @@ struct DrawingViewLayersMode: View {
         _editingState = editingState
     }
 
+    @State var isRotated = false
     var body: some View {
         ZStack {
             ForEach(indexedLayers) { indexedLayer in
+                let index = Double(indexedLayer.index)
+                let offset = Double(index - 2) * Self.offset2D
                 DrawingViewLayerButton(editingState: $editingState, layer: indexedLayer)
+                    .rotation3DEffect(
+                        isRotated ? Self.rotation : .identity,
+                        anchor: .trailing)
+                    .scaleEffect(isRotated ? Self.smallScale : Self.defaultScale)
+                    .offset(
+                        x: isRotated ? offset : 0.0,
+                        y: isRotated ? offset - 20 : 0.0
+                    )
+
             }
         }
         .offset(z: -(Double(indexedLayers.count) * CanvasLayer.layerDepth))
         .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .onAppear {
+            withAnimation {
+                isRotated = true
+            }
+        }
     }
 
     private var indexedLayers: [IndexedLayer] {
@@ -29,4 +47,10 @@ struct DrawingViewLayersMode: View {
             IndexedLayer(page: page, layer: $0.element, index: $0.offset)
         }
     }
+
+    private static let offset2D = 100.0
+
+    private static let rotation = Rotation3D(angle: Angle2D(degrees: 10), axis: RotationAxis3D(x: 0, y: 1, z: 0))
+    private static let smallScale = CGSize(width: 0.4, height: 0.4)
+    private static let defaultScale = CGSize(width: 1.0, height: 1.0)
 }
