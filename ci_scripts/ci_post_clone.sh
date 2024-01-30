227b990da5873091f81b62dc7612cf5050c42f54
Geoff Pado <geoff@pado.name>
2024-01-29 20:43:13 -0800

    Fix mise install

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 curl https://mise.jdx.dev/install.sh | sh
-export PATH="$HOME/.local/bin/bin:$PATH"
+export PATH="$HOME/.local/bin:$PATH"
 
 mise install # Installs the tools in .tool-versions
-eval "$(mise activate bash --shims)" # Adds the activated tools to $PATH
+eval "$(mise activate zsh --shims)" # Adds the activated tools to $PATH
 
 tuist fetch
 tuist generate --no-open
